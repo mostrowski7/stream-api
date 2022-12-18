6e75fa6001ed21f1e9eeb8e3b0cd45ea6f1d5d97
Michał Ostrowski <michal.ostrowski@imoli.pl>
2022-12-18 14:26:56 +0100

    feat: add env config and validation

--- users/src/modules/app/app.module.ts
+++ users/src/modules/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from 'src/config/app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from 'src/config/env.validation';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      cache: true,
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
