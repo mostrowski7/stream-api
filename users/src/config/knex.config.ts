6bbf6a3ff503105713dc380acd0ecc8571760087
Michał Ostrowski <michal.ostrowski@imoli.pl>
2022-12-25 13:24:41 +0100

    feat: create user table migration

--- users/src/config/knex.config.ts
+++ users/src/config/knex.config.ts
@@ -2,18 +2,18 @@ import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { Knex } from 'knex';
 
-config();
+config({ path: '../../.env' });
 
 const configService = new ConfigService();
 
 const knexConfig: Knex.Config = {
   client: 'postgresql',
   connection: {
-    host: configService.get<string>('database.postgresHost'),
-    port: configService.get<number>('database.postgresPort'),
-    user: configService.get<string>('database.postgresUser'),
-    password: configService.get<string>('database.postgresPassword'),
-    database: configService.get<string>('database.postgresDb'),
+    host: configService.get<string>('POSTGRES_HOST'),
+    port: configService.get<number>('POSTGRES_PORT'),
+    user: configService.get<string>('POSTGRES_USER'),
+    password: configService.get<string>('POSTGRES_PASSWORD'),
+    database: configService.get<string>('POSTGRES_DB'),
   },
   migrations: {
     extension: 'ts',
