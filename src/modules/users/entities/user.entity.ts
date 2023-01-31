3883b6a3d197e40e8c40eb933773f81ba43acd78
mostrowski7 <ursusenszaman@gmail.com>
2023-01-31 16:03:53 +0100

    fix(users): add refresh token to user entity constructor

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -5,11 +5,17 @@ export class User {
   password: string;
   refreshToken: string;
 
-  constructor(id: string, name: string, email: string, password: string) {
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    refreshToken: string,
+  ) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
-    this.refreshToken = this.refreshToken;
+    this.refreshToken = refreshToken;
   }
 }
