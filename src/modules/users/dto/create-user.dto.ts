1ed1abfc9b192ccab954538b5c47151ce459a66f
mostrowski7 <ursusenszaman@gmail.com>
2023-01-31 16:00:46 +0100

    docs: remove swagger documentation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,23 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger/dist';
 import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({
-    default: 'name',
-  })
   @IsString()
   @Length(3, 50)
   name: string;
 
-  @ApiProperty({
-    default: 'name@test.com',
-  })
   @IsEmail()
   email: string;
 
-  @ApiProperty({
-    default: 'password',
-  })
   @IsString()
   @Length(8, 50)
   password: string;
