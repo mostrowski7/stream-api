6feb35a2851c9ebf02062d00b5137c3a901750fa
mostrowski7 <ursusenszaman@gmail.com>
2023-01-31 16:48:14 +0100

    docs: remove swagger package and decorators

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,24 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import {
-  ApiCreatedResponse,
-  ApiBadRequestResponse,
-  ApiConflictResponse,
-  ApiInternalServerErrorResponse,
-  ApiTags,
-} from '@nestjs/swagger/dist';
 
-@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @ApiCreatedResponse({ description: 'User created' })
-  @ApiBadRequestResponse({ description: 'Invalid input data' })
-  @ApiConflictResponse({ description: 'User already exists' })
-  @ApiInternalServerErrorResponse({ description: 'Something went wrong' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
